efc1a649015c9b175f4e60505471ec5cf8483704
khadijakhatun79 <khadijakhatunnurse@gmail.com>
2026-05-02 11:47:43 +0600

    update

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,7 @@
+
+
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://assignment-8-wheat.vercel.app",  
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
 });
